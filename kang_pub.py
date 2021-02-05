89823a0615c070f9261f5677e54e0abf830ad0ff
adowsom <70246543+adowsom@users.noreply.github.com>
2021-02-06 02:11:37 +0300

    Update kang_pub.py

--- kang_pub.py
+++ kang_pub.py
@@ -3,9 +3,13 @@ data = {}
 for x in range(3000000):
   data[str(x)] = x
 
-for x in range(60):
+for x in range(3):
   time.sleep(3)
   print(len(data)) 
 
 
 print(data["5"]) 
+import os
+os.environ.set('last', 'lol')
+is_prod = os.environ.get('last', None)
+print(is_prod)
